7011b93728b387750b4018c77cb27ee9ab725e45
stio <dev@stio.studio>
2024-08-14 02:20:46 +0200

    origin fix

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ import * as http from "http";
 
 const server = http.createServer((request, response) => {
     response.statusCode = 200
+    response.writeHead(200, {
+        "Content-Type": "text/plain",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type",
+    });
     response.end(request.headers['cf-connecting-ip']);
 }).listen(5004);
